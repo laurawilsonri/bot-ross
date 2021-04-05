611b48d688e4f68c51b6e15e7f2b02f4c5dc9832
laurawilsonri <laura_wilson@brown.edu>
2021-04-05 16:58:54 -0400

    added unified image and label preprocessing

--- preprocess.py
+++ preprocess.py
@@ -3,6 +3,8 @@ import tensorflow as tf
 import pandas as pd
 import os
 
+
+
 '''
 Loads and converts an image to float32
 '''
@@ -15,23 +17,54 @@ def clean_image(file_path):
 
 
 '''
-Builds a dataset of images from the given directory
+    Returns a tuple of the image paths and corresponding label paths. 
+'''
+def get_data_paths(dir_path):
+
+    img_paths, label_paths = [], []
+    for f_name in os.listdir(dir_path + '/images'):
+        img_path = dir_path + '/images/' + f_name
+        label_path = dir_path + '/labels/' + f_name
+        img_paths.append(img_path)
+        label_paths.append(label_path)
+    
+    return (img_paths, label_paths)
+
+
+'''
+Loads the image and label maks. 
+'''
+def get_image_label_pair(img_path, label_path):
+  img = clean_image(img_path)
+  label = tf.io.read_file(label_path)
+  # TODO maybe clean label image ??
+
+  return img, label
+
+
+'''
+Builds a dataset of images from the given directory.
 '''
 def load_images(train_data_dir):
-    dir_path = train_data_dir + '/*.png'
-    print("LOADING IMAGES FROM: ", dir_path)
-    
-    dataset = tf.data.Dataset.list_files(dir_path)
-    dataset = dataset.shuffle(buffer_size=250000)
-    dataset = dataset.map(map_func=clean_image, num_parallel_calls=2)
-    dataset = dataset.batch(100, drop_remainder=True)
+    print("LOADING IMAGES FROM: ", train_data_dir)
+
+    data_paths = get_data_paths("data/train")
+    dataset = tf.data.Dataset.from_tensor_slices(data_paths)
+    #dataset = dataset.shuffle(buffer_size=250000)
+    dataset = dataset.map(map_func=get_image_label_pair, num_parallel_calls=2)
+    dataset = dataset.batch(5, drop_remainder=True)
     dataset = dataset.prefetch(1)
 
     return dataset
 
 
 # Example usage
-images = load_images("data/train/images")
-for image in images:
-    print(image)
+dataset = load_images("data/train")
+for imgs, labels in dataset.as_numpy_iterator():
+    # imgs is a list of batch_size of images
+    # labels is a list of batch_size of labels
+    # labels[0] is the label mask for imgs[0]
+    print("num imgs: ", len(imgs))
+    print("num labels: ", len(labels))
+    
 
